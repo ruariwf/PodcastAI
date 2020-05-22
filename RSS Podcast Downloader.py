4664699c3ec924d170779e019dd44ee0f5c982a2
ruariwf <ruari.wf@gmail.com>
2020-05-22 16:13:16 +1000

    The code is correctly printing all the download URLS next step is to get python to download and save the MP3s I also intend to be creating a persistent file that has the urls meta data and trasncriptions and AI output

--- RSS Podcast Downloader.py
+++ RSS Podcast Downloader.py
@@ -3,4 +3,15 @@ import feedparser
 rssFeed = "https://feeds.simplecast.com/6_oR5wn4"
 
 feed = feedparser.parse(rssFeed)
-print(feed.entries[0].links)
+rssEntries = feed.entries
+
+x=0
+for rows in rssEntries:
+	rssLinks = rssEntries[x].links
+	x+=1
+	for rows in rssLinks:
+		downloadLink = rssLinks[1]
+		print(downloadLink['href'])
+
+
+
