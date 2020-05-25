d3b9e0e73b58593ddd76c631d56af153c62d59f2
ruariwf <ruari.wf@gmail.com>
2020-05-25 11:11:45 +1000

    Changed to functions, added in limit and url parameters

--- RSS Podcast Downloader.py
+++ RSS Podcast Downloader.py
@@ -1,17 +1,31 @@
 import feedparser
+import requests
 
-rssFeed = "https://feeds.simplecast.com/6_oR5wn4"
 
-feed = feedparser.parse(rssFeed)
-rssEntries = feed.entries
 
-x=0
-for rows in rssEntries:
-	rssLinks = rssEntries[x].links
-	x+=1
-	for rows in rssLinks:
-		downloadLink = rssLinks[1]
-		print(downloadLink['href'])
+def episode_download(mp3_url, file_name):
+	r = requests.get(mp3_url, allow_redirects=True)
+	open(file_name + '.mp3', 'wb').write(r.content)
+
+
+def get_mp3_download(rssFeed, limit):
+	x=0
+	feed = feedparser.parse(rssFeed)
+	rssEntries = feed.entries
+	try:
+		for rows in rssEntries:
+			while x < limit:
+				rssLinks = rssEntries[x].links
+				title = rssEntries[x].title
+				x+=1
+				downloadLink = rssLinks[1]
+				for rows in downloadLink:
+					url = downloadLink['href']
+				episode_download(url, title)
+				print(title + " downloaded")
+	except: print("done")
+
+get_mp3_download("https://feeds.simplecast.com/6_oR5wn4", 3)
 
 
 
